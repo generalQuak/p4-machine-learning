77a2b93161a789846f7d61818e7f274ed40d7fee
StormDracyan <brandonngsign@live.com>
2024-11-09 12:57:44 -0500

    Started Classifer class

--- classifier.cpp
+++ classifier.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <set>
+
 using namespace std;
 
+class Classifier {
+  private:
+  map<string, int> class_counts;
+  map<string, map<string, int>> word_counts;
+  map<string, double> class_prob;
+  map<string, map<string, double>> word_likely;
+
+  public:
+  void train(const string& train_filename) {
+
+  }
+
+  string predict(const string& post) {
+    return "TBD";
+  }
+};
 
 int main() {
   cout << "Hello World!\n";
